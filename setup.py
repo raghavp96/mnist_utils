bb21fedabf754e7cd32fd338600e0cdaf7bd1cbc
Raghavprasanna Rajagopalan <raghav@Raghavprasannas-MacBook-Pro.local>
2020-07-17 19:27:00 -0400

    added build and dist to ignore file, updated package name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mnist-utils-raghavp96", # Replace with your own username
+    name="mnist-utils",
     version="0.0.1",
     author="Raghavprasanna Rajagopalan",
     author_email="raghavp96@gmail.com",
